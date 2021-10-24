0ab46cbd7eff32f7ec49a73304539b34c1f6bd0f
iferraro <afronerd01@outlook.com>
2021-10-23 19:48:29 -0700

    Added 'Armorbearer', changed 'Evidently Emerald' and 'Formidiblue', and removed 'Linear: Dark'

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const colors = [
+    {name: "Armorbearer", hex: "6F4B27"},
     {name: "Wet Sand", hex: "AEA98B"},
     {name: "Kidney Bean", hex: "B9535D"},
     {name: "Dauntless Pink", hex: "CC3399"},
@@ -8,16 +9,15 @@ const colors = [
     {name: "Honeycomb", hex: "FE9C01"},
     {name: "Pompous Orange", hex: "FFAC40"},
     {name: "Sardonic Green", hex: "8DA868"},
-    {name: "Evidently Emerald", hex: "378C40"},
+    {name: "Evidently Emerald", hex: "1DA253"},
     {name: "Tiger Beetle", hex: "188E68"},
     {name: "Swamp", hex: "204338"},
     {name: "Humble Green", hex: "00201C"},
     {name: "Cyan Spirit: Dark", hex: "003A42"},
     {name: "Sincere Blue", hex: "2626C6"},
     {name: "Hydrangea", hex: "044CE1"},
-    {name: "Linear: Dark", hex: "036595"},
-    {name: "Linear: Light", hex: "0385B5"},
-    {name: "Formidiblue", hex: "0482CD"},
+    {name: "Formidiblue", hex: "015EAA"},
+    {name: "Linear", hex: "0385B5"},
     {name: "Mail", hex: "EDEFEE"},
     {name: "Robin Egg", hex: "9ACACA"},
     {name: "Sand-ish Teal-ish", hex: "547C86"},
@@ -43,8 +43,6 @@ colors.forEach(function(c) {
     frame.append(infoFrame);
     infoFrame.hide();
     frame.click(function() {
-        // infoFrame.show();
-        // infoFrame.animate({width: "45%"}, 150);
         infoFrame.slideToggle(150);
     });
 });
